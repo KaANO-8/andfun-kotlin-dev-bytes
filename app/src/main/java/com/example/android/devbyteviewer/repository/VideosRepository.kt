9d9e121720c8fb085278684b5aa1733b62cc2be9
rahulkanojia <kaanorahul8@gmail.com>
2020-05-26 00:28:09 +0530

    added caching mechanism

--- app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -16,3 +16,28 @@
  */
 
 package com.example.android.devbyteviewer.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.example.android.devbyteviewer.database.VideoDatabase
+import com.example.android.devbyteviewer.database.asDomainModel
+import com.example.android.devbyteviewer.domain.Video
+import com.example.android.devbyteviewer.network.Network
+import com.example.android.devbyteviewer.network.asDatabaseModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class VideosRepository(private val videoDatabase: VideoDatabase) {
+
+    val videos: LiveData<List<Video>> = Transformations.map(videoDatabase.videoDao.getVideos()) {
+        it.asDomainModel()
+    }
+
+    suspend fun refreshVideos() {
+        withContext(Dispatchers.IO) {
+            val playlist = Network.devbytes.getPlaylist().await()
+            // use of spread operator to pass the values of array
+            videoDatabase.videoDao.insertAll(*playlist.asDatabaseModel())
+        }
+    }
+}
